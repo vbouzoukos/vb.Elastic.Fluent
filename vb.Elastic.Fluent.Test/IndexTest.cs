998101d8b05809cd7e11c710a1dc6ad1ae4d761a
Vasilios Bouzoukos <forgion@gmail.com>
2018-09-29 23:19:31 +0300

    .net core support

--- vb.Elastic.Fluent.Test/IndexTest.cs
+++ vb.Elastic.Fluent.Test/IndexTest.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Linq;
 using vb.Elastic.Fluent.Core;
+using Xunit;
+using System.Linq;
 using vb.Elastic.Fluent.Indexer;
 using vb.Elastic.Fluent.Search;
 
 namespace vb.Elastic.Fluent.Test
 {
-    [TestClass]
     public class IndexTest
     {
         public IndexTest()
         {
             Manager.Instance.Connect("1", "http://localhost:9200", "test");
         }
-        [TestMethod]
+        [Fact]
         public void Indexing()
         {
             IndexManager.PurgeIndexes();
@@ -27,14 +26,14 @@ namespace vb.Elastic.Fluent.Test
             IndexManager.IndexEntity(expected, true);
             var searchData = new FindRequest<SampleDocument>(0, 10);
             var results = searchData
-                .And(SearchTerm<SampleDocument>.Term(x=>x.Id, expected.Id))
+                .And(SearchTerm<SampleDocument>.Term(x => x.Id, expected.Id))
                 .Execute();
             var actual = results.Documents.FirstOrDefault();
-            Assert.AreEqual(expected.Id, actual.Id);
-            Assert.AreEqual(expected.Content, actual.Content);
-            Assert.AreEqual(expected.Title, actual.Title);
+            Assert.Equal(expected.Id, actual.Id);
+            Assert.Equal(expected.Content, actual.Content);
+            Assert.Equal(expected.Title, actual.Title);
         }
-        [TestMethod]
+        [Fact]
         public void Search()
         {
         }
